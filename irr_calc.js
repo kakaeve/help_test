6783d2d8141cdc97f3a9b5ed6599e558f36a7afe
김성호 <zkzkdlqm@gmail.com>
2021-10-17 11:51:34 +0900

    21.10.17 1차완성

--- irr_calc.js
+++ irr_calc.js
@@ -1,5 +1,5 @@
 let calcButton = document.getElementById("calc_button");
-calcButton.addEventListener('click',function(){
+calcButton.addEventListener('click', function () {
     let supply = document.getElementById("supply").value;
     supply = supply.split(',').join("");
     supply = parseInt(supply);
@@ -9,11 +9,35 @@ calcButton.addEventListener('click',function(){
     let percent = document.getElementById("percent").value;
 
     let payment_time = document.getElementById("left-toggle").classList.contains('active');//선불은 false 반대로면 true
-    
-    let calc_value = (supply * ( 1 + (percent/100) * period / 12) - (deposit/100) * supply) / period;
 
-    
-    calc_value = Math.floor(calc_value/up)*up;
+    let calc_value = (supply * (1 + (percent / 100) * period / 12) - (deposit / 100) * supply) / period;
+
+    let insurance = 0.12;
+    let RV = supply * deposit / 100;
+    let total = supply * (1 + ((percent) * (period / 12) / 100));
+    let margin_revenue = total - supply;
+    let monthly_rental_fee = (Math.round(((total - RV) / period) / 100) * 100);
+
+    let ins_total = (Math.floor(supply * (1 + insurance / 100)) + (payment_time ? monthly_rental_fee : 0)*-1);
+    let irr_total_in = 0;
+    let calc_period = period - (payment_time?1:0);
+
+    let irr_flows = new Array(calc_period+1);
+    console.log(calc_period);
+    for(let i = 0;i<calc_period;i++){
+        if((i+1)%12==0&&(i+1)!=calc_period){
+            irr_total_in += monthly_rental_fee - supply*insurance/100*insurance_free_percent;
+            irr_flows[i] = monthly_rental_fee - supply*insurance/100*insurance_free_percent;
+        }
+        else{
+            irr_total_in += monthly_rental_fee;
+            irr_flows[i] = monthly_rental_fee;
+        }
+        console.log(irr_flows[i]);
+    }
+    irr_flows[calc_period] = RV;
+
+    calc_value = Math.floor(calc_value / up) * up;
     calc_value = calc_value.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 
 
@@ -25,19 +49,20 @@ calcButton.addEventListener('click',function(){
     let span = document.getElementById("calc_value");
     span.textContent = calc_value;
 
-    let irrVal = 0.0;
+    let irr_span = document.getElementById("irr_value");
+    irr_span.textContent = irrCalc(irr_total_in + RV, ins_total, irr_flows, calc_period+1) + "%";
 
 
 
 
 }, false);
 
-document.getElementById("refresh_button").addEventListener('click', function(event) {
+document.getElementById("refresh_button").addEventListener('click', function (event) {
 
     window.location.reload();
 }, false);
 
-document.getElementById("rental_schedule").addEventListener('click',()=>{
+document.getElementById("rental_schedule").addEventListener('click', () => {
     let supply = document.getElementById("supply").value;
     supply = supply.split(',').join("");
     supply = parseInt(supply);
@@ -45,16 +70,16 @@ document.getElementById("rental_schedule").addEventListener('click',()=>{
     let period = document.getElementById("period").value;
     let percent = document.getElementById("percent").value;
 
-    let child_window = window.open("table.html","table");
- 
+    let child_window = window.open("table.html", "table");
+
     let payment_time = document.getElementById("left-toggle").classList.contains('active');
 
-    if(percent == "")
+    if (percent == "")
         percent = 0;
-    let data = supply.toString() + ' ' + deposit.toString()+ ' ' + period.toString() + ' ' + percent.toString() + ' ' + payment_time;
+    let data = supply.toString() + ' ' + deposit.toString() + ' ' + period.toString() + ' ' + percent.toString() + ' ' + payment_time;
     console.log(data);
     let push_data = document.createElement("input");
-    push_data.setAttribute("id","base-data");
+    push_data.setAttribute("id", "base-data");
     document.body.appendChild(push_data);
     document.getElementById('base-data').style.display = "none";
     document.getElementById('base-data').value = data;
@@ -62,16 +87,16 @@ document.getElementById("rental_schedule").addEventListener('click',()=>{
     // console.log(document.getElementById("base-data").value);
     // child_window.document.getElementById("base-data").value = document.getElementById("base-data").value;
     // console.log(child_window.document.getElementById("base-data").value);
-    
+
     //document.getElementById('base-data').remove();
-},false);
+}, false);
 
 
-document.getElementById("left-toggle").addEventListener('click',()=>{
+document.getElementById("left-toggle").addEventListener('click', () => {
     document.getElementById("left-toggle").className = "toggle-button left-toggle active";
     document.getElementById("right-toggle").className = "toggle-button right-toggle";
 });
-document.getElementById("right-toggle").addEventListener('click',()=>{
+document.getElementById("right-toggle").addEventListener('click', () => {
     document.getElementById("right-toggle").className = "toggle-button right-toggle active";
     document.getElementById("left-toggle").className = "toggle-button left-toggle";
 });
@@ -80,3 +105,58 @@ document.getElementById("right-toggle").addEventListener('click',()=>{
 // document.getElementsByClassName("left-toggle").addEventListener('click',()=>{
 //     document.getElementsByClassName("left-toggle").className += 'active';
 // });
+
+function irrCalc(total_in, total_out, income_per_month, period) {
+    let result;
+    let npv = 0;
+    total_in = total_in * 1.0;
+    total_out = total_out * 1.0;
+
+    //let period = income_per_month.length;
+
+    for (let i = 0; i < period + 1; i++) {
+        income_per_month[i] *= 1.0;
+        console.log(income_per_month[i]);
+    }
+
+    if (total_out == 0 || total_out > total_in) {
+        result = 0;
+    }
+    else {
+        let count = 0;
+        console.log("123");
+
+        for (result = 0; result < 100, count<100000; result = result + 0.000001, count++) {
+            //result = 0.010286999999998894;
+            npv = 0.0;
+            for (let i = 0; i < period + 1; i++) {
+                //console.log("i : " + i);
+                //console.log("result : " + result);
+                // console.log("1 + result : " + (1 + result));
+                // console.log("irr_flows[i][1] : " + irr_flows[i][1]);
+                // console.log("Math : " + Math.pow(1+result, irr_flows[i][1])*1.0);
+                // console.log("irr_flows[i][0] : " + irr_flows[i][0]/Math.pow(1+result, irr_flows[i][1]));
+
+                npv = npv + income_per_month[i] / Math.pow(1 + result, i + 1);
+
+                //console.log("npv : " + npv);
+            }
+            //console.log("npv : " + npv);
+            //console.log("result : " + result);
+            if (npv <= total_out) {
+                console.log(count);
+                console.log("out");
+                console.log(total_out);
+
+                console.log("npv : " + npv);
+                console.log("result : " + result);
+                break;
+            }
+
+        }
+    }
+    //document.getElementsByClassName("irr_target").innerHTML = Math.round(result*100, 4) + "%%";
+    result = (result * 100).toFixed(4) * 12;
+    result = Math.round(result * 100) / 100;
+    return result;
+}
--- table.js
+++ table.js
@@ -1,3 +1,5 @@
+//import irrCalc from "./irrCalc";
+
 function add_comma(text) {
     let flag = true;
     if (text < 0) {
@@ -32,11 +34,26 @@ let period = parseInt(base_data[2]);
 let percent = parseFloat(base_data[3]);
 let payment_time = base_data[4] == "true" ? true : false;
 
+
 let insurance = 0.12;
 let RV = supply * deposit / 100;
 let total = supply * (1 + ((percent) * (period / 12) / 100));
 let margin_revenue = total - supply;
 let monthly_rental_fee = (Math.round(((total - RV) / period) / 100) * 100);
+
+let insurance_free_percent = 0.6;
+
+console.log("supply : " + supply);
+console.log("total : " + total);
+
+if(payment_time==true)
+    period -= 1;
+
+let ins_total = (Math.floor(supply * (1 + insurance / 100)) + (payment_time ? monthly_rental_fee : 0)*-1);
+
+let irr_total_in = 0;
+let irr_total_out=supply;
+let irr_flows = new Array(period+1);
 
 let main_div = document.createElement('div');
 console.log(main_div);
@@ -88,12 +105,25 @@ for (let i = 0; i < small_row_length; i++) {
 
 let excel_row_length = period + 6;
 
+const excel_row_count = excel_row_length-5;
+
 let excel_table = document.createElement('table');
 let excel_thrad = document.createElement('thead');
 let excel_tbody = document.createElement('tbody');
 
 excel_table.id = "excel-table";
 excel_table.classList.add("excel-table");
+
+// let ins_total = 0;
+// for(let i=0;i<period;i++){
+//     if(i%12==0&&i!=1&&i==period-1){
+//         ins_total += monthly_rental_fee - supply*insurance/100*insurance_free_percent;
+//     }
+//     else{
+//         ins_total += monthly_rental_fee;
+//     }
+// }
+// ins_total /= supply/period;
 
 
 
@@ -126,6 +156,7 @@ for(let i=0;i<excel_row_data[0].length;i++)
 
 excel_thrad.appendChild(excel_row[0]);
 
+
 excel_row[1] = document.createElement('tr');
 excel_row_data[1][1] = document.createElement('td');
 excel_row_data[1][0] = document.createElement('td');
@@ -136,7 +167,7 @@ excel_row_data[1][5] = document.createElement('td');
 excel_row_data[1][0].innerHTML = "";
 excel_row_data[1][1].innerHTML = "";
 excel_row_data[1][2].innerHTML = "";
-excel_row_data[1][3].innerHTML = add_comma((Math.floor(supply * (1 + insurance / 100)) + (payment_time ? 0 : monthly_rental_fee)) * -1);
+excel_row_data[1][3].innerHTML = add_comma(ins_total*-1);
 excel_row_data[1][4].innerHTML = "";
 excel_row_data[1][5].innerHTML = add_comma(-1 * supply);
 excel_row[1].appendChild(excel_row_data[1][0]);
@@ -146,7 +177,11 @@ excel_row[1].appendChild(excel_row_data[1][3]);
 excel_row[1].appendChild(excel_row_data[1][4]);
 excel_row[1].appendChild(excel_row_data[1][5]);
 
+
+
+
 excel_tbody.appendChild(excel_row[1]);
+
 
 for (let i = 2; i < excel_row_length - 4; i++) {
     excel_row[i] = document.createElement('tr');
@@ -164,7 +199,16 @@ for (let i = 2; i < excel_row_length - 4; i++) {
                 excel_row_data[i][j].innerHTML = i - 1;
                 break;
             case 3:
-                excel_row_data[i][j].innerHTML = add_comma(monthly_rental_fee);
+                if((i-1)%12==0&&excel_row_length-6!=i-1){
+                    excel_row_data[i][j].innerHTML = add_comma(monthly_rental_fee - supply*insurance/100*insurance_free_percent);
+                    irr_total_in += monthly_rental_fee - supply*insurance/100*insurance_free_percent;
+                    irr_flows[i-2] = monthly_rental_fee - supply*insurance/100*insurance_free_percent;
+                }
+                else{
+                    excel_row_data[i][j].innerHTML = add_comma(monthly_rental_fee);
+                    irr_total_in += monthly_rental_fee;
+                    irr_flows[i-2] = monthly_rental_fee;
+                }
                 break;
             case 4:
                 excel_row_data[i][j].innerHTML = i - 1;
@@ -173,12 +217,14 @@ for (let i = 2; i < excel_row_length - 4; i++) {
                 excel_row_data[i][j].innerHTML = add_comma(monthly_rental_fee);
                 break;
         }
+        console.log(irr_total_in);
         excel_row_data[i][j].classList.add("number-content");
         excel_row[i].appendChild(excel_row_data[i][j]);
     }
     excel_tbody.appendChild(excel_row[i]);
 }
 
+irr_flows[period] = RV*1.0;
 
 excel_row[excel_row_length-4] = document.createElement('tr');
 excel_row_data[excel_row_length-4][1] = document.createElement('td');
@@ -206,7 +252,7 @@ excel_row[excel_row_length-4].appendChild(excel_row_data[excel_row_length-4][5])
 
 excel_tbody.appendChild(excel_row[excel_row_length-4]);
 
-
+console.log("supply : " + supply);
 
 excel_row[excel_row_length-3] = document.createElement('tr');
 excel_row_data[excel_row_length-3][0] = document.createElement('td');
@@ -218,7 +264,7 @@ excel_row_data[excel_row_length-3][5] = document.createElement('td');
 excel_row_data[excel_row_length-3][0].innerHTML = "회사준비금";
 excel_row_data[excel_row_length-3][1].innerHTML = add_comma(monthly_rental_fee*period);
 excel_row_data[excel_row_length-3][2].innerHTML = "IRR";
-excel_row_data[excel_row_length-3][3].innerHTML = "";
+excel_row_data[excel_row_length-3][3].innerHTML = irrCalc(irr_total_in + RV, ins_total, irr_flows, period)+"%";
 excel_row_data[excel_row_length-3][4].innerHTML = "회사준비금";
 excel_row_data[excel_row_length-3][5].innerHTML = add_comma(monthly_rental_fee*period);
 
@@ -243,11 +289,11 @@ excel_row_data[excel_row_length-2][3] = document.createElement('td');
 excel_row_data[excel_row_length-2][4] = document.createElement('td');
 excel_row_data[excel_row_length-2][5] = document.createElement('td');
 excel_row_data[excel_row_length-2][0].innerHTML = "총수입비율";
-excel_row_data[excel_row_length-2][1].innerHTML = (monthly_rental_fee*period/supply).toString() + "%";
+excel_row_data[excel_row_length-2][1].innerHTML = (monthly_rental_fee*period/supply*100).toFixed(4).toString() + "%";
 excel_row_data[excel_row_length-2][2].innerHTML = "";
 excel_row_data[excel_row_length-2][3].innerHTML = "";
 excel_row_data[excel_row_length-2][4].innerHTML = "총수입비율";
-excel_row_data[excel_row_length-2][5].innerHTML = (monthly_rental_fee*period/supply).toString() + "%";
+excel_row_data[excel_row_length-2][5].innerHTML = (monthly_rental_fee*period/supply*100).toFixed(4).toString() + "%";
 
 excel_row_data[excel_row_length-2][1].classList.add("number-content");
 excel_row_data[excel_row_length-2][3].classList.add("number-content");
@@ -270,11 +316,13 @@ excel_row_data[excel_row_length-1][3] = document.createElement('td');
 excel_row_data[excel_row_length-1][4] = document.createElement('td');
 excel_row_data[excel_row_length-1][5] = document.createElement('td');
 excel_row_data[excel_row_length-1][0].innerHTML = "월간회수율";
-excel_row_data[excel_row_length-1][1].innerHTML = (monthly_rental_fee/supply).toString() + "%";
+excel_row_data[excel_row_length-1][1].innerHTML = (monthly_rental_fee/supply*100).toFixed(4).toString() + "%";
 excel_row_data[excel_row_length-1][2].innerHTML = "Ins";
 excel_row_data[excel_row_length-1][3].innerHTML = insurance;
 excel_row_data[excel_row_length-1][4].innerHTML = "월간회수율";
-excel_row_data[excel_row_length-1][5].innerHTML = (monthly_rental_fee/supply).toString() + "%";
+excel_row_data[excel_row_length-1][5].innerHTML = (monthly_rental_fee*period/supply*100/period).toFixed(4).toString() + "%";
+
+irr_total_in = monthly_rental_fee/supply*100*supply;
 
 excel_row_data[excel_row_length-1][1].classList.add("number-content");
 excel_row_data[excel_row_length-1][3].classList.add("number-content");
@@ -294,3 +342,109 @@ excel_table.appendChild(excel_tbody);
 
 document.getElementById("main-div").appendChild(excel_table);
 
+
+
+// function irrCalc(total_in, total_out, income_per_month){
+//     let result;
+//     let npv = 0;
+//     irr_total_in += RV;
+//     irr_total_in = irr_total_in*1.0;
+//     irr_total_out = irr_total_out*1.0;
+//     console.log(typeof(irr_total_in));
+//     console.log(typeof(irr_total_out));
+//     console.log("total in : " + irr_total_in);
+//     console.log("total out : " + irr_total_out);
+//     for(let i=0;i<period+1;i++){
+//         irr_flows[i][0] *= 1.0;
+//         irr_flows[i][1] *= 1.0;
+//     }
+//     if(irr_total_out==0||irr_total_out>irr_total_in){
+//         result = 0;
+//     }
+//     else{
+//         console.log("123");
+//         let count = 0;
+//         let count2 = 0;
+
+//         for(result=0;result<100;result = result + 0.000001){
+//             npv = 0;
+//             for(let i = 0;i<period+1;i++){
+//                 // console.log("i : " + i);
+//                 //console.log("result : " + result);
+//                 // console.log("1 + result : " + (1 + result));
+//                 // console.log("irr_flows[i][1] : " + irr_flows[i][1]);
+//                 // console.log("Math : " + Math.pow(1+result, irr_flows[i][1])*1.0);
+//                 // console.log("irr_flows[i][0] : " + irr_flows[i][0]/Math.pow(1+result, irr_flows[i][1]));
+                
+//                 npv = npv + irr_flows[i][0]/Math.pow(1+result, irr_flows[i][1]);
+//                 //console.log("npv : " + npv);
+//             }
+//             console.log("npv : " + npv);
+//             console.log("result : " + result);
+//             if(npv <= irr_total_out){
+//                 break;
+//             }
+                
+//         }
+//     }
+//     //document.getElementsByClassName("irr_target").innerHTML = Math.round(result*100, 4) + "%%";
+//     return (result*100).toFixed(4)*12;
+// }
+
+function irrCalc(total_in, total_out, income_per_month, period){
+    let result;
+    let npv = 0;
+
+    console.log(period);
+    console.log(income_per_month.length);
+    total_in = total_in*1.0;
+    total_out = total_out*1.0;
+
+    //let period = income_per_month.length;
+
+    for(let i=0;i<period+1;i++){
+        income_per_month[i] *= 1.0;
+        console.log(income_per_month[i]);
+    }
+
+    if(total_out==0||total_out>total_in){
+        result = 0;
+    }
+    else{
+        let count = 0;
+        console.log("123");
+
+        for(result=0;result<100;result = result + 0.000001, count++){
+            //result = 0.010286999999998894;
+            npv = 0.0;
+            for(let i = 0;i<period+1;i++){
+                //console.log("i : " + i);
+                //console.log("result : " + result);
+                // console.log("1 + result : " + (1 + result));
+                // console.log("irr_flows[i][1] : " + irr_flows[i][1]);
+                // console.log("Math : " + Math.pow(1+result, irr_flows[i][1])*1.0);
+                // console.log("irr_flows[i][0] : " + irr_flows[i][0]/Math.pow(1+result, irr_flows[i][1]));
+                
+                npv = npv + income_per_month[i]/Math.pow(1+result,i+1);
+
+                //console.log("npv : " + npv);
+            }
+            //console.log("npv : " + npv);
+            //console.log("result : " + result);
+            if(npv <= total_out){
+                console.log(count);
+                console.log("out");
+                console.log(total_out);
+
+                console.log("npv : " + npv);
+            console.log("result : " + result);
+                break;
+            }
+                
+        }
+    }
+    //document.getElementsByClassName("irr_target").innerHTML = Math.round(result*100, 4) + "%%";
+    result = (result*100).toFixed(4)*12;
+    result = Math.round(result*100)/100;
+    return result;
+}
